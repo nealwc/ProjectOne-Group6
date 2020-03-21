b90807dc2084cefabd5c30ec7d409b93ccac26fa
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-03-20 21:08:11 -0400

    js vars for ingredients; added ids to html

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-var movie = "big";
-var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+$(".randomRecipe").on("click", function (event) {
+    event.preventDefault()
+    var ingrOne = $("#ingredient-one")
+    .val()
+    .trim();
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-    console.log(response);
+    var ingrTwo = $("#ingredient-two")
+    .val()
+    .trim();
+
+    var ingrThree = $("#ingredient-three")
+    .val()
+    .trim();
+
+    console.log(ingrOne);
+    console.log(ingrTwo);
+    console.log(ingrThree);
+
     
+    // var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    // }).then(function (response) {
+    //     console.log(response);
+
+    // });
+
 });
+
+
