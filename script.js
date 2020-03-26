bda7cee8fe7759da9a77c5f8e436ef14362ead4f
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-03-25 22:51:52 -0400

    added id's to html, cocktaildb pulls three drinks from spirit input

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
-$(".randomRecipe").on("click", function (event) {
+$("#submit-spirit").on("click", function (event) {
     event.preventDefault()
-    var ingredient= $("#ingredient-one")
-    .val()
-    .trim();
+    var spirit = $("#spiritInput")
+        .val()
+        .trim();
 
-    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient
+    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + spirit
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
+        // $("#drink-results").empty();
+        for (i = 0; i < 3; i++) {
+            var recipeID = response.drinks[i].idDrink;
+            var recipeJawn = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + recipeID;
+            console.log(recipeJawn);
 
-        var recipeID = response.drinks[0].idDrink;
-        var recipeJawn = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + recipeID;
-        console.log(recipeJawn);
+            $.ajax({
+                url: recipeJawn,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response.drinks[0].strDrink);
 
-        $.ajax({
-            url: recipeJawn,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response.drinks[0].strDrink);
-        })
-       
+                var drinkName = response.drinks[0].strDrink;
+                var drinkImg = response.drinks[0].strDrinkThumb;
+                var drinkInstruct = response.drinks[0].strInstructions;
+                var listDrink = $("<li>");
+                
+                $("#drink-results").append(drinkName);
+                $("#drink-results").append(drinkInstruct);
+
+
+
+
+
+            })
+        }
+
 
     });
 
