78535912a094e046cae2d743e1f582c06e2e7625
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-03-26 20:22:56 -0400

    added random cocktail API

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 $("#submit-spirit").on("click", function (event) {
     event.preventDefault()
-    var spirit = $("#spiritInput")
+    var cocktail = $("#spiritInput") // grabs user input for specific cocktail
         .val()
         .trim();
 
-    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + spirit
+    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + cocktail
 
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function (response) {
-        console.log(response);
+    }).then(function (response) { // only returns cocktail name, photo and id
+        console.log(response); 
         $("#drink-results").empty();
-        for (i = 0; i < 3; i++) {
-            var recipeID = response.drinks[i].idDrink;
-            var recipeJawn = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + recipeID;
-            console.log(recipeJawn);
+
+            var recipeID = response.drinks[0].idDrink; // grabs API cocktail id needed for cocktail data API
+            var recipeData = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + recipeID;
+            console.log(recipeData);
 
             $.ajax({
-                url: recipeJawn,
+                url: recipeDate,
                 method: "GET"
             }).then(function (response) {
                 console.log(response.drinks[0].strDrink);
@@ -38,38 +38,65 @@ $("#submit-spirit").on("click", function (event) {
                 $("drink-results").append(img);
 
 
-
-
-
-            })
-        }
+            });
 
 
     });
 
 });
 
+$("#submit-spirit").on("click", function (event) {
+    event.preventDefault()
+    $("#drink-results").empty();
 
-$("#submit-movie").on("click", function (event) {
-    event.preventDefault();
-    $("#movie-results").empty();
-    var movie = $("#movie-title")
-        .val()
-        .trim();
-
-    var movieAPIKey = "trilogy"
-    var movieURL = "http://www.omdbapi.com/?apikey=" + movieAPIKey + "&t=" + movie
+    var randomDrink = "https://www.thecocktaildb.com/api/json/v1/1/random.php"; // API for random cocktail
+    console.log(randomDrink);
 
     $.ajax({
-        url: movieURL,
+        url: randomDrink,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
-        var movieTitle = response.Title;
-        var listMovie = $("<li>");
-    
-        listMovie.text(movieTitle);
-        $("#movie-results").append(listMovie);
-    
+        console.log(response.drinks[0].strDrink);
+
+        var drinkName = response.drinks[0].strDrink;
+        var drinkImg = response.drinks[0].strDrinkThumb;
+        var drinkInstruct = response.drinks[0].strInstructions;
+        var drinkEl = $("<div>");
+        var instructEl = $("<div>")
+        var img = $("<img>")
+
+        drinkEl.text("Cocktail: " + drinkName);
+        instructEl.text("Instructions: " + drinkInstruct);
+        img.attr("src", drinkImg);
+        $("#drink-results").append(drinkEl);
+        $("#drink-results").append(instructEl);
+        $("drink-results").append(img);
+
+
     });
+
 });
+
+// $("#submit-movie").on("click", function (event) {
+//     event.preventDefault();
+//     $("#movie-results").empty();
+//     var movie = $("#movie-title")
+//         .val()
+//         .trim();
+
+//     var movieAPIKey = "trilogy"
+//     var movieURL = "http://www.omdbapi.com/?apikey=" + movieAPIKey + "&t=" + movie
+
+//     $.ajax({
+//         url: movieURL,
+//         method: "GET"
+//     }).then(function (response) {
+//         console.log(response);
+//         var movieTitle = response.Title;
+//         var listMovie = $("<li>");
+
+//         listMovie.text(movieTitle);
+//         $("#movie-results").append(listMovie);
+
+//     });
+// });
