1232f55ff30f017e9e152d59562435c079502f04
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-03-27 21:51:23 -0400

    repaired drink search api; created displayDrink function

--- 2script.js
+++ 2script.js
@@ -2,7 +2,8 @@ $("#submitMovie").on("click", function (event) {
     event.preventDefault()
     $("#mov1").empty();
     var parameter = $("#movieParameter")
-        .val();
+        .val()
+        .trim();
 
     console.log(parameter);
 
@@ -46,25 +47,43 @@ $("#submitMovie").on("click", function (event) {
 $(".random-movie").on("click", function (event) {
     event.preventDefault()
     $("#mov1").empty();
-    var randomIMDBid = Math.floor(Math.random() * 1999999);
-    console.log(randomIMDBid);
+    generateRandomMovie();
 
-    var queryURL = "https://www.omdbapi.com/?t=&apikey=trilogy&i=tt" + randomIMDBid;
-    console.log(queryURL);
-    
+    function generateRandomMovie() {
+        var randomIMDBid = Math.floor(Math.random() * 1999999);
+        console.log(randomIMDBid);
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+        var queryURL = "https://www.omdbapi.com/?t=&apikey=trilogy&i=tt" + randomIMDBid;
+        console.log(queryURL);
 
-        $("#mov1").append(`<div>
+
+
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            var type = response.Type;
+            var rating = parseInt(response.Metascore);
+            console.log(rating);
+            
+
+            if (response.Type === "movie" && rating > 5.0) {
+
+                $("#mov1").append(`<div>
             <h3> ${"Title: " + response.Title} </h3>
             <h3> ${"Year Released: " + response.Year} </h3>
             <h3>${"Rating: " + response.Rated} </h3>
             <h3>${response.Plot}</h3>
             <img src=${response.Poster}/>
             </div>`);
-    });
+            }
+
+            else {
+                generateRandomMovie()
+            }
+
+        });
+    }
 });
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(".cocktail-button").on("click", function (event) {
+$("#submitrecipe").on("click", function (event) {
     event.preventDefault()
     $("#drink1").empty();
     var cocktail = $(".known-cocktail") // grabs user input for specific known cocktail
@@ -7,44 +7,41 @@ $(".cocktail-button").on("click", function (event) {
 
     var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + cocktail
 
+
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function (response) { // only returns cocktail name, photo and id
+    }).then(function (response) {
         console.log(response);
 
-        var recipeID = response.drinks[0].idDrink; // grabs API cocktail id needed for cocktail data API
-        var recipeData = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + recipeID;
-        console.log(recipeData);
+        var drinkName = response.drinks[0].strDrink;
+        var drinkImg = response.drinks[0].strDrinkThumb;
+        var drinkInstruct = response.drinks[0].strInstructions;
+        var drinkEl = $("<div>");
+        var instructEl = $("<div>")
+        var img = $("<img>")
 
-        $.ajax({
-            url: recipeDate,
-            method: "GET"
-        }).then(function (response) {
-
-            var drinkName = response.drinks[0].strDrink;
-            var drinkImg = response.drinks[0].strDrinkThumb;
-            var drinkInstruct = response.drinks[0].strInstructions;
-            var drinkEl = $("<div>");
-            var instructEl = $("<div>")
-            var img = $("<img>")
-    
-            drinkEl.text("Cocktail: " + drinkName);
-            instructEl.text("Instructions: " + drinkInstruct);
-            img.attr("src", drinkImg);
-            $("#drink1").append(drinkEl);
-            $("#drink1").append(instructEl);
-            $("#drink1").append(img);
-
-
-        });
-
-
+        drinkEl.text("Cocktail: " + drinkName);
+        instructEl.text("Instructions: " + drinkInstruct);
+        img.attr("src", drinkImg);
+        $("#drink1").append(drinkEl);
+        $("#drink1").append(instructEl);
+        $("#drink1").append(img);
     });
 
 });
 
 $(".random-drink-button").on("click", function (event) {
+  displayDrink();
+
+});
+
+$(".randomAll").on("click", function (event) {
+    displayDrink();
+  
+  });
+
+function displayDrink() {
     event.preventDefault()
     $("#drink1").empty();
 
@@ -72,29 +69,4 @@ $(".random-drink-button").on("click", function (event) {
 
 
     });
-
-});
-
-// $("#submit-movie").on("click", function (event) {
-//     event.preventDefault();
-//     $("#movie-results").empty();
-//     var movie = $("#movie-title")
-//         .val()
-//         .trim();
-
-//     var movieAPIKey = "trilogy"
-//     var movieURL = "http://www.omdbapi.com/?apikey=" + movieAPIKey + "&t=" + movie
-
-//     $.ajax({
-//         url: movieURL,
-//         method: "GET"
-//     }).then(function (response) {
-//         console.log(response);
-//         var movieTitle = response.Title;
-//         var listMovie = $("<li>");
-
-//         listMovie.text(movieTitle);
-//         $("#movie-results").append(listMovie);
-
-//     });
-// });
+};
